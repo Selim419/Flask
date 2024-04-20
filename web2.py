8817888a90be0ff7d8643458be44a5b5c3d0ec61
Selim419 <134213066+Selim419@users.noreply.github.com>
2024-04-20 14:47:05 +0300

    Add files via upload

---
+++ web2.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def list_html_files(directory):
+    html_files = []
+    if os.path.exists(directory):
+        for file in os.listdir(directory):
+            if file.endswith(".html"):
+                html_files.append(file)
+    return html_files
+
+@app.route('/')
+def index():
+    web_directory = '/sdcard/Download/Hack/Web_Tools/templates/'
+    html_files = list_html_files(web_directory)
+    return render_template("index.html", html_files=html_files)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=False)
